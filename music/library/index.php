413efe72119013a29477d55e4ba640fdea6b4e5f
Oscar Jansson <oscja313@student.liu.se>
2025-10-10 14:43:54 +0200

    more things

---
+++ music/library/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>COOL ALBUMS</title>
+  <script src="./assets/script.js" type="module"></script>
+  <script src="./assets/cursor.js"></script>
+  <script>
+    function myFunction() {
+      document.getElementById("this").innerHTML = `<iframe src="http://coolinggrooling.online" style="width:100%;height:1000px"></iframe>`;
+    }
+  </script>
+  <style>
+  </style>
+  <link rel="stylesheet" href="../../assets/cursedStyle.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+</head>
+<body id="grad1">
+    <p class="books"> This is the library</p>
+</body>
