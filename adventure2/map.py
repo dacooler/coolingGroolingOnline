4d4ecb69fdea837e0200b21109daec1b7abc0c58
dacoolest <oscar.jansson1@proton.me>
2026-02-04 15:27:28 +0000

    SKYBOX FOR REAL

--- adventure2/map.py
+++ adventure2/map.py
@@ -1,9 +1,9 @@
-def buildPart(cords, size):
+def buildPart(cords, size, clas):
   style  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "width: " + size[0] + "px; height: " + size[1] + "px"  
   styleCubed  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "--width: " + size[0] + "px; --height: " + size[1] + "px; --depth: " + size[2] + "px"  
   part = "<div style=\"" + style + "\"></div>"
   cubes = "<div></div>"*6
-  partCubed = "<div class=\"cubed\" style=\"" + styleCubed + "\">" + cubes + "</div>"
+  partCubed = "<div class=\"cubed " + clas + "\" style=\"" + styleCubed + "\">" + cubes + "</div>"
   return part, partCubed
 
 def getMap():
@@ -16,7 +16,8 @@ def getMap():
     parts = cube.split(";")
     cords = parts[0].split(",")
     size = parts[1].split(",")
-    addCube, addCubed = buildPart(cords, size)
+    clas = parts[2]
+    addCube, addCubed = buildPart(cords, size, clas)
     cubesDone += addCube
     cubesCubed += addCubed
   map.close()
