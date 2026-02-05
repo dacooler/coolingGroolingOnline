051d74f9c8173e8714dcba7447291a24f95283eb
dacoolest <oscar.jansson1@proton.me>
2026-02-05 09:02:00 +0000

    some changes to map

--- adventure2/map.py
+++ adventure2/map.py
@@ -1,10 +1,14 @@
 def buildPart(cords, size, clas):
-  style  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "width: " + size[0] + "px; height: " + size[1] + "px"  
-  styleCubed  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "--width: " + size[0] + "px; --height: " + size[1] + "px; --depth: " + size[2] + "px"  
-  part = "<div style=\"" + style + "\"></div>"
-  cubes = "<div></div>"*6
-  partCubed = "<div class=\"cubed " + clas + "\" style=\"" + styleCubed + "\">" + cubes + "</div>"
-  return part, partCubed
+    style  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "width: " + size[0] + "px; height: " + size[1] + "px"  
+    part = "<div style=\"" + style + "\"></div>"
+
+    styleCubed  = " left: " + cords[0] + "px; top: " + cords[1] + "px; " + "--width: " + size[0] + "px; --height: " + size[1] + "px; --depth: " + size[2] + "px;"  
+    if(len(size) >= 4):
+        styleCubed += " --extra: " + size[3] + ";"
+
+    cubes = "<div></div>"*6
+    partCubed = "<div class=\"cubed " + clas + "\" style=\"" + styleCubed + "\">" + cubes + "</div>"
+    return part, partCubed
 
 def getMap():
   map = open("src/map.txt")
@@ -17,8 +21,12 @@ def getMap():
     cords = parts[0].split(",")
     size = parts[1].split(",")
     clas = parts[2]
+        
+
     addCube, addCubed = buildPart(cords, size, clas)
-    cubesDone += addCube
+    if not (len(parts) >= 4 and parts[3] == "noCol"):
+        cubesDone += addCube
+
     cubesCubed += addCubed
   map.close()
   return cubesDone, cubesCubed
