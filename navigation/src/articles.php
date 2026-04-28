f3b19bf387d9bc1492484da7b2970e36f8c68783
dacoolest <oscar.jansson1@proton.me>
2026-04-28 20:44:23 +0200

    hej

--- navigation/src/articles.php
+++ navigation/src/articles.php
@@ -1,6 +1,6 @@
 
         <div class="floater">
           <a href="../articles/" class="article">
-              <iframe class="articlePage" src="../articles/sanningen.html"></iframe>
+              <iframe class="articlePage" src="../articles/sanningen/"></iframe>
           </a>
         </div>
